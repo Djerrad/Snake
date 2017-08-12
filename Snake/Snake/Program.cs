3f52557c8eb78d6649ba173023da4532ea205470
Djerrad <djerrad.kz@gmail.com>
2017-08-12 21:41:41 +1000

    Созданпустой файл программы

---
+++ Snake/Snake/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: связной
+ * Date: 12.08.2017
+ * Time: 21:35
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Snake
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
